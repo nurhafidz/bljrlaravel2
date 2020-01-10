5598ebf5adf676719b30e11fd758061331975038
Psyduck08 <52746313+Psyduck08@users.noreply.github.com>
2020-01-10 08:02:56 +0700

    permulaan

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function home()
+    {
+        return view('index');
+    }
+    public function about()
+    {
+        return view('about',['nama'=>'about']);
+    }
+    public function portofolio()
+    {
+        return view('portofolio');
+    }
+}
---
+++ resources/views/portofolio.blade.php
@@ -0,0 +1,17 @@
+@extends('layout/main')
+
+@section('title','portofolio')
+
+@section('container')
+    <div class="cointainer">
+        <div class="bg-secondary text-white">
+            <div class="d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <!-- Content here -->
+                <div class="text-center text-monospace" style="font-size: 100px">
+                    Hello portofolio
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
